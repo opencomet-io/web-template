836e475ff0700536f3ef7ee5943bea6209dbdc6b
Igor Michalak <git@igormichalak.com>
2023-02-10 23:54:55 +0100

    feat: add ExternalLink atom component

---
+++ src/components/atoms/ExternalLink/ExternalLink.tsx
@@ -0,0 +1,30 @@
+import type { FC, ReactNode } from 'react';
+
+import * as S from './ExternalLink.styles';
+
+export interface Props {
+  children: ReactNode;
+  destination: string;
+  newTab?: boolean;
+}
+
+const ExternalLink: FC<Props> = ({
+  children,
+  destination,
+  newTab = true,
+  ...props
+}) => {
+  return (
+    <S.Anchor
+      href={destination}
+      target={newTab ? '_blank' : '_self'}
+      referrerPolicy="same-origin"
+      rel="noopener noreferrer"
+      {...props}
+    >
+      {children}
+    </S.Anchor>
+  );
+};
+
+export default ExternalLink;
