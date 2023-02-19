47e8b410f8e2088eaa9952ebf3a25730370681b9
Igor Michalak <git@igormichalak.com>
2023-02-19 12:15:14 +0100

    feat: add alt locales URL generation to the next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,23 @@
+const i18nextConfig = require('./next-i18next.config');
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   sitemapSize: 7000,
   exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
-    additionalSitemaps: [`${process.env.SITE_URL}/server-sitemap.xml`],
+    additionalSitemaps: [
+      `${process.env.NEXT_PUBLIC_SITE_URL}/server-sitemap.xml`,
+    ],
   },
+  alternateRefs: i18nextConfig.i18n.locales.flatMap(locale =>
+    locale === i18nextConfig.i18n.defaultLocale
+      ? []
+      : {
+          hreflang: locale,
+          href: process.env.NEXT_PUBLIC_SITE_URL + `/${locale}`,
+        }
+  ),
 };
--- src/pages/server-sitemap.xml/index.tsx
+++ src/pages/server-sitemap.xml/index.tsx
@@ -1,15 +1,8 @@
 import { GetServerSideProps } from 'next';
 import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
 
-import env from 'utils/env';
-
 export const getServerSideProps: GetServerSideProps = async ctx => {
-  const fields: Array<ISitemapField> = [
-    {
-      loc: env.SITE_URL || 'https://example.com',
-      lastmod: new Date().toISOString(),
-    },
-  ];
+  const fields: Array<ISitemapField> = [];
 
   return getServerSideSitemap(ctx, fields);
 };
